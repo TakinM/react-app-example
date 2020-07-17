61b6a648eea1f8ca1cbd45ece979a8932ea0570a
Emma Marley <takinmarley@Emmas-MacBook-Pro.local>
2020-07-17 20:25:26 +0200

    updating fish and updating order

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,14 @@
+import Rebase from "re-base";
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyCmDokPhhmgG6llxY_psifk14BHMtXvVAs",
+  authDomain: "hub-marketplace2020.firebaseapp.com",
+  databaseURL: "https://hub-marketplace2020.firebaseio.com",
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+export { firebase };
+
+export default base;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,12 +4,43 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
+import base from "../base";
+import EditFishForm from "./EditFishForm";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {},
   };
+
+  componentDidMount() {
+    const { params } = this.props.match;
+
+    //first resinate our localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+
+    if (localStorageRef) {
+      this.setState({
+        order: JSON.parse(localStorageRef),
+      });
+    }
+
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes",
+    });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem(
+      this.props.match.params.storeId,
+      JSON.stringify(this.state.order)
+    );
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
 
   addFish = (fish) => {
     console.log(fish);
@@ -27,12 +58,47 @@ class App extends React.Component {
     this.setState({ fishes: sampleFishes });
   };
 
+  updateFish = (key, updatedFish) => {
+    //1. take a copy of the current state
+    const fishes = { ...this.state.fishes };
+
+    //2. update that state
+    fishes[key] = updatedFish;
+
+    //3. set updated fish into state
+    this.setState({
+      fishes,
+    });
+  };
+
+  deleteFish = (key) => {
+    //1. take a copy of state
+    const fishes = { ...this.state.fishes };
+
+    //2. update the state
+    fishes[key] = null;
+
+    //3. update the state
+    this.setState({ fishes });
+  };
+
   addToOrder = (key) => {
     //1. take a copy of state
     const order = { ...this.state.order };
 
     //2. either add to the order or update numner in our order
     order[key] = order[key] + 1 || 1;
+
+    //3. call setState to update our state object with the order
+    this.setState({ order });
+  };
+
+  removeFromOrder = (key) => {
+    //1. take a copy of state
+    const order = { ...this.state.order };
+
+    //2. remove that item from the order
+    delete order[key];
 
     //3. call setState to update our state object with the order
     this.setState({ order });
@@ -54,14 +120,23 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          removeFromOrder={this.removeFromOrder}
+        />
         <Inventory
           addFish={this.addFish}
+          updateFish={this.updateFish}
+          deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
+          fish={this.state.fishes}
         />
       </div>
     );
   }
 }
+
+//this.props is only used in a component where a component is not defined
 
 export default App;
